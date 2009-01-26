90c8aad4732126d568e28be737a9d534bcf79fc0
Alvin Schur <alvin.schur@gmail.com>
2009-01-26 09:03:29 -0700

    minor doc update

--- features/step_definitions/ajax_steps.rb
+++ features/step_definitions/ajax_steps.rb
@@ -11,7 +11,7 @@ When "I visit a page making an AJAX call returning javascript and eval the respo
 end
 
 When "I wait for the AJAX call to finish" do
-  # TODO: use new syntax with the soon-to-be released version of celerity
+  # TODO: use new syntax with the edge version of celerity
   # @browser.join_threads
   @browser.page.getEnclosingWindow().getThreadManager().joinAll(10000)
 end
